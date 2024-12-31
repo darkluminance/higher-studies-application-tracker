0826743d2ab538fdcfd3b8a24c7ca3060f3945f6
Raiyan Abrar <201914013@student.mist.ac.bd>
2025-01-01 00:58:54 +0600

    Update formpage UI

--- solid-js-frontend/src/components/ItemInputBase.tsx
+++ solid-js-frontend/src/components/ItemInputBase.tsx
@@ -1,15 +1,27 @@
+import { createMemo } from "solid-js";
+import BackIcon from "./Icons/BackIcon";
+
 export default function ItemInputBase(props: any) {
+	const formName = createMemo(() => {
+		const formType = props.editData ? "Edit" : "Create";
+		const formComponentName = props.component?.name
+			.replace("[solid-refresh]", "")
+			.replace("Form", "");
+
+		return formType + " " + formComponentName;
+	});
 	return (
 		<div>
-			<div class="max-w-[1000px] p-8">
-				<div class="text-base flex justify-start mb-8">
+			<div class="max-w-[1000px] p-16">
+				<div class="text-base flex justify-start">
 					<span
 						class="cursor-pointer font-bold hover:opacity-50"
 						onClick={props.closeFunction}
 					>
-						{"<- "}Back
+						<BackIcon width="48px" fill="#333"></BackIcon>
 					</span>
 				</div>
+				<div class="my-8 text-2xl">{formName()}</div>
 				<div>
 					{props.component && (
 						<props.component
